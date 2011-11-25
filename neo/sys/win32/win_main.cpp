c46cda33494e9a46fc5af9b48a2b805f520ee275
Leith Bade <leith@leithalweapon.geek.nz>
2011-11-25 23:51:45 +1300

    Updated Windows version detection to support Windows 7.

--- neo/sys/win32/win_main.cpp
+++ neo/sys/win32/win_main.cpp
@@ -1007,8 +1007,10 @@ void Sys_Init( void ) {
 			win32.sys_arch.SetString( "Win2K (NT)" );
 		} else if( win32.osversion.dwMajorVersion == 5 && win32.osversion.dwMinorVersion == 1 ) {
 			win32.sys_arch.SetString( "WinXP (NT)" );
-		} else if ( win32.osversion.dwMajorVersion == 6 ) {
-			win32.sys_arch.SetString( "Vista" );
+		} else if ( win32.osversion.dwMajorVersion == 6 && win32.osversion.dwMinorVersion == 0 ) {
+			win32.sys_arch.SetString( "WinVista (NT)" );
+		} else if ( win32.osversion.dwMajorVersion == 6 && win32.osversion.dwMinorVersion == 1 ) {
+			win32.sys_arch.SetString( "Win7 (NT)" );
 		} else {
 			win32.sys_arch.SetString( "Unknown NT variant" );
 		}
