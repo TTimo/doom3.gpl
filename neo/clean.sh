1158b6a57105377509bfe3a41dfbb1b5b2b9914a
hawake <hawake@gmail.com>
2011-12-05 22:46:01 +0100

    adding clean script for linux

---
+++ neo/clean.sh
@@ -0,0 +1,55 @@
+# Clean script for Linux OS by hawake
+#!/bin/sh
+rm -r debug
+rm -r release
+rm quake3.ncb
+rm quake3.opt
+rm quake3.plg
+rm quake3.stt
+rm -r cgame/debug
+rm -r cgame/release
+rm cgame/cgame.ncb
+rm cgame/cgame.opt
+rm cgame/cgame.plg
+rm cgame/cgame.stt
+rm -r game/debug
+rm game/release
+rm game/game.ncb
+rm game/game.opt
+rm game/game.plg
+rm game/game.stt
+rm -r ui/debug
+rm -r ui/release
+rm ui/ui.ncb
+rm ui/ui.opt
+rm ui/ui.plg
+rm ui/ui.stt
+rm -r renderer/debug
+rm -r renderer/release
+rm renderer/renderer.ncb
+rm renderer/renderer.opt
+rm renderer/renderer.plg
+rm renderer/renderer.stt
+rm -r botlib/debug
+rm -r botlib/release
+rm botlib/botlib.ncb
+rm botlib/botlib.opt
+rm botlib/botlib.plg
+rm botlib/botlib.stt
+rm -r botlai/debug
+rm -r botlai/release
+rm botai/botai.dsp
+rm botai/botai.plg
+rm -r bspc/debug
+rm -r bspc/release
+rm bspc/bspc.exe
+rm bspc/bspc.log
+rm bspc/bspc.ncb
+rm bspc/bspc.opt
+rm bspc/bspc.pdb
+rm bspc/bspc.plg
+rm -r unix/debugi386-glibc
+rm -r unix/releasei386-glibc 
+rm -r mac/MacQuake3\ Data/
+rm -r macosx/Client/Q3Test.app
+rm -r macosx/Client/Q3Test.build
