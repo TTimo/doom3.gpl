a725daf17e598400e95a1558b4081d24ec8cec5b
XoD <xoddark@gmail.com>
2011-12-21 21:48:50 +0100

    Fix a test in idAI::Event_CanBecomeSolid
    It's probably a small bug, idBarrel	and idExplodingBarrel can by tested
    event if spawnClearMoveables is false.
    
    Signed-off-by: XoD <xoddark@gmail.com>

--- neo/d3xp/ai/AI_events.cpp
+++ neo/d3xp/ai/AI_events.cpp
@@ -876,7 +876,7 @@ void idAI::Event_CanBecomeSolid( void ) {
 		}
 
 #ifdef _D3XP
-		if ( spawnClearMoveables && hit->IsType( idMoveable::Type ) || hit->IsType( idBarrel::Type ) || hit->IsType( idExplodingBarrel::Type ) ) {
+		if ( spawnClearMoveables && ( hit->IsType( idMoveable::Type ) || hit->IsType( idBarrel::Type ) || hit->IsType( idExplodingBarrel::Type ) ) ) {
 			idVec3 push;
 			push = hit->GetPhysics()->GetOrigin() - GetPhysics()->GetOrigin();
 			push.z = 30.f;
